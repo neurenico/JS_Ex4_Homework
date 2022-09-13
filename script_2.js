20228add5b35cbcd83fe2a7c11511087ce93b724
Nikita <neurenico@gmail.com>
2022-09-13 10:04:43 +0300

    fixed bug

--- script_2.js
+++ script_2.js
@@ -1,11 +1,11 @@
-let counter = 0;
+let counterOfMovies = 0;
 function hanoiTowers(height, from, to, buf) {
 
     if (height > 0) {
 
     hanoiTowers(height - 1, from, buf, to);
     console.log('Move disk ' + height + ' from Tower ' + from + ' to Tower ' + to);
-    counter++;
+    counterOfMovies++;
     hanoiTowers(height - 1, buf, to, from);
     }
     
@@ -16,5 +16,5 @@ console.log('Hanoi towers');
 console.log('--------------------------');
 hanoiTowers(4, "1", "2", "3");
 console.log('--------------------------');
-console.log('Count of movies ' + counter);
+console.log('Count of movies ' + counterOfMovies);
 
